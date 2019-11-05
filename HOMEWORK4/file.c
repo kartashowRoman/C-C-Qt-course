1548b500cdb38b6c24815d1808d0aefddcc4f368
kartashowRoman <56906299+kartashowRoman@users.noreply.github.com>
2019-11-05 21:19:12 +0300

    Update file.c

--- HOMEWORK4/file.c
+++ HOMEWORK4/file.c
@@ -5,24 +5,27 @@
 
 
 void main(){
-	srand(time(NULL));
-		
+
+	srand(time(NULL));	
 	long int numbers = rand();
+	long int count[1000];
+	int k;
+	for (k = 0; k <1001; k++) {
+		count[k] = numbers;
+	}
 	
 
-	
+
 	FILE *fp;
 	for (int i; i < 11; i++){
 	fp = fopen(i+"randnum.txt", "w");
 	
-	
-	for(int j = 0; j < 10000; j++) fprintf(fp, "%li", numbers);
+	for(int j = 0; j < 100; j++) fprintf(fp, "%li", count[1000]);
 	
 	fclose(fp);
 	}
 	
 
 }
-
 
 
